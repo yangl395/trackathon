29c1af17afdcff51274f8eb926263272b5bdb7d8
Gianluca Pane <Luke@Gianlucas-MacBook-Pro.local>
2015-02-08 01:58:11 -0500

    fullpage

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
-var app = require('express')();
+var express = require('express');
+var app = express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
+
+app.use('/bower_components',  express.static(__dirname + '/bower_components'));
 
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/views/index.html');
